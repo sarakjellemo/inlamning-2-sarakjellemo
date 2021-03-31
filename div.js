2783e6ac0f93cfc598484f7f11d2fdf3099c30f5
sarakjellemo <77279576+sarakjellemo@users.noreply.github.com>
2021-03-31 12:41:52 +0200

    added div.js and div.test.js test succeded

---
+++ div.js
@@ -0,0 +1,3 @@
+let div = (num1, num2) => num1 - num2; 
+
+module.exports = div;
---
+++ div.test.js
@@ -0,0 +1,5 @@
+const div = require ("./div"); 
+
+test("dividerar 6 - 4 to equal ", () => {
+    expect(div(6, 4)).toBe(2);
+});
